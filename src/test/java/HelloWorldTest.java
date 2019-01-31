e4bf092dad631c5690a1d7c83a347738af7c9f78
Yuseff Perry <yuseff.perry@capgemini.com>
2019-01-31 16:36:33 -0600

    Cleaned src folder up adding test and changing Jenkinsfile a bit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *  Compilation:  javac HelloWorldTest.java
+ *  Execution:    java HelloWorldTest
+ *
+ *  Prints "Hello, World Test". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorldTest
+ *  Hello, World Test
+ *
+ *  These 17 lines of text are comments. They are not part of the program;
+ *  they serve to remind us about its properties. The first two lines tell
+ *  us what to type to compile and test the program. The next line describes
+ *  the purpose of the program. The next few lines give a sample execution
+ *  of the program and the resulting output. We will always include such 
+ *  lines in our programs and encourage you to do the same.
+ *
+ ******************************************************************************/
+
+public class HelloWorldTest {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World Test");
+    }
+
+}
